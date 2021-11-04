6264a3ed99202ed4881632a71f0d0c513bd1281d
Dennis Sumser <dennis.sumser@solox.de>
2021-11-04 17:03:07 +0100

    added react router 6

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import {
+  Routes,
+  Route,
+  Link,
+  useNavigate,
+  useLocation,
+  Navigate,
+} from "react-router-dom";
+
+import { HomePage, Layout, LoginPage, ProfilePage } from "./pages";
+import { useAuth } from "./context/auth";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route
+        path="/"
+        element={
+          <RequireAuth>
+            <Layout />
+          </RequireAuth>
+        }
+      >
+        <Route index element={<HomePage />} />
+        <Route path="profile" element={<ProfilePage />} />
+      </Route>
+      <Route path="/login" element={<LoginPage />} />
+    </Routes>
   );
+}
+
+function RequireAuth({ children }) {
+  let { user } = useAuth();
+  let location = useLocation();
+
+  if (!user) {
+    // Redirect them to the /login page, but save the current location they were
+    // trying to go to when they were redirected. This allows us to send them
+    // along to that page after they login, which is a nicer user experience
+    // than dropping them off on the home page.
+    return <Navigate to="/login" state={{ from: location }} />;
+  }
+
+  return children;
 }
 
 export default App;
