628397ed6d1f1b374ed45559f9b31aa4b391c97a
Dennis Sumser <dennis.sumser@solox.de>
2021-11-05 23:53:39 +0100

    display loading button at login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -49,6 +49,7 @@ export const Page = ({ className }) => {
           </div>
           <div>
             <button
+              disabled={loginInProcess}
               type="submit"
               className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
             >
@@ -58,7 +59,7 @@ export const Page = ({ className }) => {
                   aria-hidden="true"
                 />
               </span>
-              Sign in
+              {loginInProcess ? "Loading" : "Sign in"}
             </button>
           </div>
         </form>
