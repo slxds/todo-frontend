14870b7e06956fa9daa40aac33ee56ae77c76dc3
Dennis Sumser <dennis.sumser@solox.de>
2021-11-05 22:24:51 +0100

    added toasts

--- src/pages/Layout.jsx
+++ src/pages/Layout.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigate } from "react-router-dom";
 import {
   CalendarIcon,
   ChartBarIcon,
@@ -11,7 +11,15 @@ import {
 import { useTodo } from "../context/todo";
 import { useAuth } from "../context/auth";
 
-import { MobileSidebar, Sidebar, TopBar } from "../components";
+import {
+  MobileSidebar,
+  Modal,
+  Notification,
+  Sidebar,
+  TopBar,
+} from "../components";
+import "react-toastify/dist/ReactToastify.css";
+import { ToastContainer, toast } from "react-toastify";
 
 const navigation = [
   { name: "Dashboard", href: "#", icon: HomeIcon, current: false },
@@ -31,11 +39,6 @@ const navigation = [
   { name: "Documents", href: "#", icon: InboxIcon, current: false },
   { name: "Reports", href: "#", icon: ChartBarIcon, current: false },
 ];
-const userNavigation = [
-  { name: "Your Profile", href: "#" },
-  { name: "Settings", href: "#" },
-  { name: "Sign out", href: "#" },
-];
 
 export const Layout = () => {
   const { logout } = useAuth();
@@ -43,20 +46,30 @@ export const Layout = () => {
 
   const [lists, setLists] = useState([]);
 
+  const navigate = useNavigate();
   const { createNewToDoList, myToDoLists } = useTodo();
 
-  const onCreateNewList = () => {
-    createNewToDoList();
+  const onCreateNewList = async () => {
+    let newId = await createNewToDoList();
+    if (!newId) return;
+    navigate(`/${newId}`);
   };
+
+  const userNavigation = [
+    { name: "Your Profile", onClick: () => navigate("/profile") },
+    { name: "Sign out", onclick: () => logout() },
+  ];
 
   return (
     <>
       <div className="flex flex-row h-full">
         <MobileSidebar
           setSidebarOpen={setSidebarOpen}
+          onCreateNewList={onCreateNewList}
           sidebarOpen={sidebarOpen}
-          navigation={navigation}
+          navigation={myToDoLists}
         />
+
         <Sidebar navigation={myToDoLists} onCreateNewList={onCreateNewList} />
         <div className="flex flex-col flex-1">
           <TopBar
@@ -66,6 +79,14 @@ export const Layout = () => {
           <main>
             <div className="py-6">
               <div className="mx-auto px-4 sm:px-6 md:px-8">
+                <ToastContainer
+                  position="bottom-right"
+                  autoClose={5000}
+                  hideProgressBar={false}
+                  newestOnTop={true}
+                  closeOnClick
+                  pauseOnFocusLoss={false}
+                />
                 <Outlet />
               </div>
             </div>
