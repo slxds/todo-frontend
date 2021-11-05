e74216000e5e27d4faff15e8ae643abd63baf305
Dennis Sumser <dennis.sumser@solox.de>
2021-11-05 22:24:04 +0100

    added modal and notification comp

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,5 @@
 export { Component as Sidebar } from "./Sidebar";
 export { Component as MobileSidebar } from "./MobileSidebar";
 export { Component as TopBar } from "./TopBar";
+export { Component as Modal } from "./Modal";
+export { Component as Notification } from "./Notification";
