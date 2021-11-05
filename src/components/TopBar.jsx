23d39667877fcb97fe9c7da39d8f77b66c98f7a2
Dennis Sumser <dennis.sumser@solox.de>
2021-11-05 22:25:54 +0100

    switched user icon

--- src/components/TopBar.jsx
+++ src/components/TopBar.jsx
@@ -52,11 +52,20 @@ export const Component = ({ setSidebarOpen, userNavigation }) => {
             <div>
               <Menu.Button className="max-w-xs bg-white flex items-center text-sm rounded-full focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
                 <span className="sr-only">Open user menu</span>
-                <img
+                <span className="inline-block h-8 w-8 rounded-full overflow-hidden bg-gray-100">
+                  <svg
+                    className="h-full w-full text-gray-300"
+                    fill="currentColor"
+                    viewBox="0 0 24 24"
+                  >
+                    <path d="M24 20.993V24H0v-2.996A14.977 14.977 0 0112.004 15c4.904 0 9.26 2.354 11.996 5.993zM16.002 8.999a4 4 0 11-8 0 4 4 0 018 0z" />
+                  </svg>
+                </span>
+                {/*<img
                   className="h-8 w-8 rounded-full"
                   src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
                   alt=""
-                />
+                />*/}
               </Menu.Button>
             </div>
             <Transition
@@ -72,15 +81,15 @@ export const Component = ({ setSidebarOpen, userNavigation }) => {
                 {userNavigation.map((item) => (
                   <Menu.Item key={item.name}>
                     {({ active }) => (
-                      <a
-                        href={item.href}
+                      <button
+                        onClick={item.onClick}
                         className={classNames(
                           active ? "bg-gray-100" : "",
-                          "block px-4 py-2 text-sm text-gray-700"
+                          "block px-4 py-2 text-sm text-gray-700 text-left w-full"
                         )}
                       >
                         {item.name}
-                      </a>
+                      </button>
                     )}
                   </Menu.Item>
                 ))}
