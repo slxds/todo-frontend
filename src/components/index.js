c8039a9111ea4f1df7b72dbefa5208a37873ac01
Dennis Sumser <dennis.sumser@solox.de>
2021-11-07 17:38:45 +0100

    export created components

--- src/components/index.js
+++ src/components/index.js
@@ -3,3 +3,5 @@ export { Component as MobileSidebar } from "./MobileSidebar";
 export { Component as TopBar } from "./TopBar";
 export { Component as Modal } from "./Modal";
 export { Component as Notification } from "./Notification";
+export { OutlinedHeroIcon, SolidHeroIcon } from "./HeroIcon";
+export { Component as DropDownMenu } from "./DropDownMenu";
