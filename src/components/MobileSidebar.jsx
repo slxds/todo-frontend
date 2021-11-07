e497d9ab25ec0e0f0d740272062920b903230336
Dennis Sumser <dennis.sumser@solox.de>
2021-11-07 17:36:47 +0100

    moved modal

--- src/components/MobileSidebar.jsx
+++ src/components/MobileSidebar.jsx
@@ -21,84 +21,14 @@ export const Component = ({
   setSidebarOpen,
   navigation,
   onCreateNewList,
+  setSubmitErrorModalOpen,
 }) => {
   let { listId } = useParams();
 
-  let { reportError } = useTodo();
-
-  const [open, setOpen] = useState(false);
-  const [message, setMessage] = useState("");
-
   const navigate = useNavigate();
-
-  const onReportError = (e) => {
-    e.preventDefault();
-    reportError({ text: message })
-      .then((res) => {
-        setOpen(false);
-        setMessage("");
-        toast.success("Thank you for your feedback");
-      })
-      .catch((err) => toast.error("Sorry that did not work"));
-  };
 
   return (
     <>
-      <Modal open={open} setOpen={setOpen}>
-        <form onSubmit={onReportError}>
-          <div className="sm:flex sm:items-start">
-            <div className="mx-auto flex-shrink-0 flex items-center justify-center h-12 w-12 rounded-full bg-blue-100 sm:mx-0 sm:h-10 sm:w-10">
-              <QuestionMarkCircleIcon
-                className="h-6 w-6 text-blue-600"
-                aria-hidden="true"
-              />
-            </div>
-            <div className="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left w-full">
-              <Dialog.Title
-                as="h3"
-                className="text-lg leading-6 font-medium text-gray-900"
-              >
-                Fehler melden
-              </Dialog.Title>
-              <div className="mt-2 w-full">
-                <label
-                  htmlFor="about"
-                  className="block text-sm font-medium text-gray-700"
-                >
-                  Beschreibe den Fehler kurz
-                </label>
-                <div className="mt-1 w-full">
-                  <textarea
-                    id="about"
-                    value={message}
-                    onChange={(e) => setMessage(e.target.value)}
-                    rows={3}
-                    className="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 mt-1 block w-full sm:text-sm border border-gray-300 rounded-md"
-                    placeholder="Tell us something"
-                    defaultValue={""}
-                    required
-                  />
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className="mt-5 sm:mt-4 sm:flex sm:flex-row-reverse">
-            <button
-              type="submit"
-              className="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-red-600 text-base font-medium text-white hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500 sm:ml-3 sm:w-auto sm:text-sm"
-            >
-              Send
-            </button>
-            <button
-              type="button"
-              className="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:w-auto sm:text-sm"
-              onClick={() => setOpen(false)}
-            >
-              Cancel
-            </button>
-          </div>
-        </form>
-      </Modal>
       <Transition.Root show={sidebarOpen} as={Fragment}>
         <Dialog
           as="div"
@@ -212,7 +142,7 @@ export const Component = ({
                   <button
                     onClick={() => {
                       setSidebarOpen(false);
-                      setOpen(true);
+                      setSubmitErrorModalOpen(true);
                     }}
                     type="button"
                     className="inline-flex items-center p-1 border border-transparent rounded-full shadow-sm text-white  hover:bg-indigo-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -21,82 +21,15 @@ function classNames(...classes) {
   return classes.filter(Boolean).join(" ");
 }
 
-export const Component = ({ navigation, onCreateNewList }) => {
+export const Component = ({
+  navigation,
+  onCreateNewList,
+  setSubmitErrorModalOpen,
+}) => {
   let { listId } = useParams();
-
-  const [open, setOpen] = useState(false);
-  const [message, setMessage] = useState("");
-
-  const { reportError } = useTodo();
-
-  const onReportError = (e) => {
-    e.preventDefault();
-    reportError({ text: message })
-      .then((res) => {
-        setOpen(false);
-        setMessage("");
-        toast.success("Thank you for your feedback");
-      })
-      .catch((err) => toast.error("Sorry that did not work"));
-  };
 
   return (
     <>
-      <Modal open={open} setOpen={setOpen}>
-        <form onSubmit={onReportError}>
-          <div className="sm:flex sm:items-start">
-            <div className="mx-auto flex-shrink-0 flex items-center justify-center h-12 w-12 rounded-full bg-blue-100 sm:mx-0 sm:h-10 sm:w-10">
-              <QuestionMarkCircleIcon
-                className="h-6 w-6 text-blue-600"
-                aria-hidden="true"
-              />
-            </div>
-            <div className="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left w-full">
-              <Dialog.Title
-                as="h3"
-                className="text-lg leading-6 font-medium text-gray-900"
-              >
-                Fehler melden
-              </Dialog.Title>
-              <div className="mt-2 w-full">
-                <label
-                  htmlFor="about"
-                  className="block text-sm font-medium text-gray-700"
-                >
-                  Beschreibe den Fehler kurz
-                </label>
-                <div className="mt-1 w-full">
-                  <textarea
-                    id="about"
-                    value={message}
-                    onChange={(e) => setMessage(e.target.value)}
-                    rows={3}
-                    className="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 mt-1 block w-full sm:text-sm border border-gray-300 rounded-md"
-                    placeholder="Tell us something"
-                    defaultValue={""}
-                    required
-                  />
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className="mt-5 sm:mt-4 sm:flex sm:flex-row-reverse">
-            <button
-              type="submit"
-              className="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-red-600 text-base font-medium text-white hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500 sm:ml-3 sm:w-auto sm:text-sm"
-            >
-              Send
-            </button>
-            <button
-              type="button"
-              className="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:w-auto sm:text-sm"
-              onClick={() => setOpen(false)}
-            >
-              Cancel
-            </button>
-          </div>
-        </form>
-      </Modal>
       <div className="hidden md:flex md:w-64 md:flex-col md:inset-y-0">
         {/* Sidebar component, swap this element with another sidebar if you like */}
         <div className="flex flex-col flex-grow pt-5 bg-indigo-700 overflow-y-auto overflow-x-hidden">
@@ -202,7 +135,7 @@ export const Component = ({ navigation, onCreateNewList }) => {
           <div className="flex-shrink-0 flex border-t border-indigo-800 p-4 items-center">
             <div className="flex-1">
               <button
-                onClick={() => setOpen(true)}
+                onClick={() => setSubmitErrorModalOpen(true)}
                 type="button"
                 className="inline-flex items-center p-1 border border-transparent rounded-full shadow-sm text-white  hover:bg-indigo-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
               >
