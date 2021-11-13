c936f89b1b4cd62095b82e4dcca2979da45a7dd2
Dennis Sumser <dennis.sumser@solox.de>
2021-11-13 23:11:43 +0100

    make sidebar useable

--- src/modals/TodoListItemDetails.jsx
+++ src/modals/TodoListItemDetails.jsx
@@ -4,21 +4,57 @@ import {
   PlusSmIcon,
   QuestionMarkCircleIcon,
 } from "@heroicons/react/solid";
-export const TodoListItemDetails = ({ open, setOpen, item }) => {
-  const team = [
-    {
-      name: "Tom Cook",
-      email: "tomcook@example.com",
-      href: "#",
-      imageUrl:
-        "https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
-    },
-  ];
+import { useEffect, useState, useRef } from "react";
+import { useTodo } from "../context/todo";
+
+const team = [
+  {
+    name: "Tom Cook",
+    email: "tomcook@example.com",
+    href: "#",
+    imageUrl:
+      "https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
+  },
+];
+
+export const TodoListItemDetails = ({ open, setOpen, item, listId }) => {
+  const [name, setName] = useState("");
+  const [description, setDescription] = useState("");
+
+  const textAreaRef = useRef(null);
+
+  const { editToDoItem } = useTodo();
+
+  useEffect(() => {
+    if (!item) return;
+
+    setName(item.text);
+    setDescription(item.description);
+  }, [item]);
+
+  useEffect(() => {
+    if (!textAreaRef.current) return;
+    textAreaRef.current.style.height = "30px";
+    const scrollHeight = textAreaRef.current.scrollHeight;
+    textAreaRef.current.style.height = 2 + scrollHeight + "px";
+  }, [name]);
+
+  const onFormSubmit = (e) => {
+    e.preventDefault();
+    editToDoItem(listId, item._id, { text: name, description: description })
+      .then((res) => {
+        console.log(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
   return (
     <SlideOver
       open={open}
       setOpen={setOpen}
-      onFormSubmit={() => {}}
+      onFormSubmit={onFormSubmit}
       title=""
       subtext=""
     >
@@ -26,10 +62,13 @@ export const TodoListItemDetails = ({ open, setOpen, item }) => {
         <div className="space-y-6  pb-5">
           <div>
             <div className="mt-1">
-              <input
+              <textarea
+                ref={textAreaRef}
                 type="text"
-                value={item?.text}
+                value={name}
+                onChange={(e) => setName(e.target.value)}
                 name="project-name"
+                rows={1}
                 id="project-name"
                 className="block w-full shadow-sm sm:text-sm focus:ring-indigo-500 focus:border-indigo-500 border-gray-300 rounded-md"
               />
@@ -47,8 +86,9 @@ export const TodoListItemDetails = ({ open, setOpen, item }) => {
                 id="description"
                 name="description"
                 rows={4}
+                onChange={(e) => setDescription(e.target.value)}
                 className="block w-full shadow-sm sm:text-sm focus:ring-indigo-500 focus:border-indigo-500 border border-gray-300 rounded-md"
-                defaultValue={""}
+                value={description}
               />
             </div>
           </div>
