1dfcf2d4231693e981e5650c771d1db1d2c80914
Dennis Sumser <dennis.sumser@solox.de>
2021-11-13 23:10:06 +0100

    add new functions

--- src/context/todo/Provider.jsx
+++ src/context/todo/Provider.jsx
@@ -7,6 +7,7 @@ export const Provider = ({ children }) => {
   const { jwt } = useAuth();
 
   const [todoLists, setTodoLists] = useState([]);
+  const [me, setMe] = useState(null);
 
   const todo = axios.create({
     baseURL: process.env.REACT_APP_BASEURL,
@@ -15,12 +16,14 @@ export const Provider = ({ children }) => {
 
   useEffect(() => {
     if (!jwt) return;
+    getMe();
+    console.log(jwt);
     getMyToDoLists();
   }, [jwt]);
 
   const getMyToDoLists = () => {
     todo
-      .get(`/v1.0/todo`)
+      .get(`/v1/todo`)
       .then((res) => {
         setTodoLists(res.data);
       })
@@ -29,7 +32,7 @@ export const Provider = ({ children }) => {
 
   const createNewToDoList = () => {
     return todo
-      .post("/v1.0/todo")
+      .post("/v1/todo")
       .then((res) => {
         getMyToDoLists();
         return res.data._id;
@@ -39,20 +42,42 @@ export const Provider = ({ children }) => {
       });
   };
 
+  const getMe = () => {
+    todo
+      .get(`/v1/profile/me`)
+      .then((res) => {
+        setMe(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
   const contextValue = {
     myToDoLists: todoLists,
+    me: me,
     refreshToDoList: getMyToDoLists,
     createNewToDoList: createNewToDoList,
     changeToDoListName: (listId, data) =>
-      todo.patch(`/v1.0/todo/name/${listId}`, data),
-    getToDoList: (listId) => todo.get(`/v1.0/todo/${listId}`),
-    addToDoItem: (listId, data) => todo.post(`/v1.0/todo/${listId}`, data),
+      todo.patch(`/v1/todo/name/${listId}`, data),
+    getToDoList: (listId) => todo.get(`/v1/todo/${listId}`),
+    addToDoItem: (listId, data) => todo.post(`/v1/todo/${listId}`, data),
+    editToDoItem: (listId, itemId, data) =>
+      todo.patch(`/v1/todo/details/${listId}/${itemId}`, data),
     checkToDoItem: (listId, itemId, done = false) =>
-      todo.patch(`/v1.0/todo/${listId}/${itemId}`, { done: done }),
-    deletetoDoList: (listId) => todo.delete(`/v1.0/todo/${listId}`),
-    reportError: (data) => todo.post("/v1.0/issue", data),
-    getMe: () => todo.get(`/v1.0/profile/me`),
-    updateProfile: (data) => todo.patch(`/v1.0/profile/me`, data),
+      todo.patch(`/v1/todo/check/${listId}/${itemId}`, { done: done }),
+    deletetoDoList: (listId) => todo.delete(`/v1/todo/${listId}`),
+    reportError: (data) => todo.post("/v1/issue", data),
+    getProfileImage: (userId) => todo.get(`/v1/profile/${userId}/image`),
+    updateProfile: (data) =>
+      todo.patch(`/v1/profile/me`, data, {
+        headers: {
+          Authorization: "Bearer " + jwt,
+          "Content-Type": "multipart/form-data",
+        },
+      }),
+    updateMe: () => getMe(),
+    shareList: (listId, data) => todo.post(`/v1/todo/share/${listId}`, data),
   };
 
   return <Context.Provider value={contextValue}>{children}</Context.Provider>;
