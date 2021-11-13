5eb9869da7917a5993d25ed1e355908d7663ffd3
Dennis Sumser <dennis.sumser@solox.de>
2021-11-13 23:08:51 +0100

    add profile picture

--- src/components/TopBar.jsx
+++ src/components/TopBar.jsx
@@ -2,12 +2,67 @@ import { Fragment } from "react";
 import { Menu, Transition } from "@headlessui/react";
 import { BellIcon, MenuAlt2Icon } from "@heroicons/react/outline";
 import { SearchIcon } from "@heroicons/react/solid";
+import { useAuth } from "../context/auth";
+import { useTodo } from "../context/todo";
 
 function classNames(...classes) {
   return classes.filter(Boolean).join(" ");
 }
 
 export const Component = ({ setSidebarOpen, userNavigation }) => {
+  const { me } = useTodo();
+
+  const ProfileImage = () => {
+    if (!me) {
+      return (
+        <span className="inline-block h-8 w-8 rounded-full overflow-hidden bg-gray-100">
+          <svg
+            className="h-full w-full text-gray-300"
+            fill="currentColor"
+            viewBox="0 0 24 24"
+          >
+            <path d="M24 20.993V24H0v-2.996A14.977 14.977 0 0112.004 15c4.904 0 9.26 2.354 11.996 5.993zM16.002 8.999a4 4 0 11-8 0 4 4 0 018 0z" />
+          </svg>
+        </span>
+      );
+    } else if (me.image) {
+      return (
+        <img
+          className="h-8 w-8 rounded-full"
+          src={`http://localhost:4000/v1/file/public/${me.image}`}
+          alt=""
+        />
+      );
+    } else if (me.name) {
+      let p = "";
+      if (me.name.indexOf(" ") > 0) {
+        p = me.name.split(" ")[0][0] + me.name.split(" ")[1][0];
+      } else {
+        p = me.name[0];
+      }
+
+      return (
+        <span className="inline-flex items-center justify-center h-8 w-8 rounded-full bg-gray-500">
+          <span className="text-sm font-medium leading-none text-white">
+            {p.toUpperCase()}
+          </span>
+        </span>
+      );
+    } else {
+      return (
+        <span className="inline-block h-8 w-8 rounded-full overflow-hidden bg-gray-100">
+          <svg
+            className="h-full w-full text-gray-300"
+            fill="currentColor"
+            viewBox="0 0 24 24"
+          >
+            <path d="M24 20.993V24H0v-2.996A14.977 14.977 0 0112.004 15c4.904 0 9.26 2.354 11.996 5.993zM16.002 8.999a4 4 0 11-8 0 4 4 0 018 0z" />
+          </svg>
+        </span>
+      );
+    }
+  };
+
   return (
     <div className="sticky top-0 z-10 flex-shrink-0 flex h-16 bg-white shadow">
       <button
@@ -52,7 +107,8 @@ export const Component = ({ setSidebarOpen, userNavigation }) => {
             <div>
               <Menu.Button className="max-w-xs bg-white flex items-center text-sm rounded-full focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
                 <span className="sr-only">Open user menu</span>
-                <span className="inline-block h-8 w-8 rounded-full overflow-hidden bg-gray-100">
+                <ProfileImage />
+                {/* <span className="inline-block h-8 w-8 rounded-full overflow-hidden bg-gray-100">
                   <svg
                     className="h-full w-full text-gray-300"
                     fill="currentColor"
@@ -61,7 +117,7 @@ export const Component = ({ setSidebarOpen, userNavigation }) => {
                     <path d="M24 20.993V24H0v-2.996A14.977 14.977 0 0112.004 15c4.904 0 9.26 2.354 11.996 5.993zM16.002 8.999a4 4 0 11-8 0 4 4 0 018 0z" />
                   </svg>
                 </span>
-                {/*<img
+                <img
                   className="h-8 w-8 rounded-full"
                   src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
                   alt=""
